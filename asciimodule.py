71169d2b3c31b4ba02e95f155a67bb81eb72d8b3
rejishragvan <ragavanrejish@gmail.com>
2023-10-23 11:47:17 +0530

    adding ascii functions

---
+++ asciimodule.py
@@ -0,0 +1,17 @@
+def asciifunc():
+    #print character for a ascii values
+    print("characters for ascii values")
+    for i in range(127):
+        print(i,chr(i))
+
+
+    #ascii value for a character
+    print("ascii values for characters")
+    l = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5',
+         '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+         'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a',
+         'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+         'x', 'y', 'z', '{', '|', '}', '~']
+    for i in l:
+        print(i, ord(i))
+
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import listmodule
 import tuplemodule
 import setmodule
 import dictionarymodule
+import asciimodule
 
 def main():
 #    listmodule.listfunc()
 #    tuplemodule.tuplefunc()
 #    setmodule.setfunction()
-     dictionarymodule.dictionaryfunc()
+#     dictionarymodule.dictionaryfunc()
+    asciimodule.asciifunc()
 
 if __name__=="__main__":
     main()
